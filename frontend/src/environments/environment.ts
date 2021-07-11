8c1be88bf64b9da5fe77afd57c1a1a5af0b60dc9
Mahdi <mahdihassan1@hotmail.com>
2021-07-12 02:35:27 +0300

    commit

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
+  apiServerUrl: ' https://fsndcaps.herokuapp.com/', // the running FLASK api server url
   auth0: {
     url: 'udacafe.eu', // the auth0 domain prefix
     audience: 'coffeeshop', // the audience set for the auth0 app
